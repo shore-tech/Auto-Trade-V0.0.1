e503b976eb8c745b41df20f929af1992266ab776
Donald Ho <dh3coding@gmail.com>
2024-11-10 14:46:48 +1100

    init_db script

--- psql_script/golden_cross_es/initiate.sql
+++ psql_script/golden_cross_es/initiate.sql
@@ -1,6 +1,16 @@
 -- create user
-CREATE USER newuser WITH PASSWORD 1234;
+CREATE USER newuser WITH PASSWORD '1234';
 
 
 -- create database
 CREATE DATABASE demo_db OWNER newuser;
+
+-- create schema
+\c demo_db;
+CREATE SCHEMA golden_cross_es;
+
+
+-- grant permission
+GRANT CREATE ON SCHEMA public TO newuser;
+GRANT ALL PRIVILEGES ON DATABASE demo_db TO newuser;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA golden_cross_es TO newuser;
